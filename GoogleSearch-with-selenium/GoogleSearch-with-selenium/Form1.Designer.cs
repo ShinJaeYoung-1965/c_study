ebf79bbca6fc992c47531dee06e8805c6b171dd3
ShinJaeYoung-1965 <odsy@chol.com>
2021-01-11 17:18:13 +0900

    2021.01.11 작업

---
+++ GoogleSearch-with-selenium/GoogleSearch-with-selenium/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GoogleSearch_with_selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(565, 99);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 25);
+            this.btn_search.TabIndex = 0;
+            this.btn_search.Text = "검색";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(70, 99);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(474, 25);
+            this.txt_search.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "검색어";
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(646, 100);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 25);
+            this.btn_Close.TabIndex = 3;
+            this.btn_Close.Text = "종료";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(15, 158);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(1024, 474);
+            this.txtDisplay.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 679);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.btn_search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ GoogleSearch-with-selenium/GoogleSearch-with-selenium/Form1.cs
@@ -0,0 +1,111 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleSearch_with_selenium
+{
+
+    public partial class Form1 : Form
+    {
+        //private ChromeDriverService _driverService = null;
+        //private ChromeOptions _options = null;
+        //private ChromeDriver _driver = null;
+        //IWebDriver driver = new ChromeDriver();
+        private ChromeDriverService service = ChromeDriverService.CreateDefaultService("D:/1개인/비업무용_Study/C#/Reference");
+        private ChromeOptions option = new ChromeOptions();
+        
+            //List<GoldenCross> goldens = new List<GoldenCross>();
+
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            service.HideCommandPromptWindow = true;
+            //option.AddArguments("headless");
+            option.AddArgument("disable-gpu");
+            //_driverService = ChromeDriverService.CreateDefaultService("D:/1개인/비업무용_Study/C#/Reference");
+            //_driverService.HideCommandPromptWindow = true;
+
+            //_options = new ChromeOptions();
+            //_options.AddArgument("disable-gpu");
+            ////_options.AddArgument("headless");
+            //_driver = new ChromeDriver(_driverService, _options);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            //_driver.Navigate().GoToUrl(" http://172.31.7.80:8081/WS_ACK.asmx?WSDL");
+            //_driver.Navigate().GoToUrl("https://google.co.kr");
+            //_driver.Manage().Window.Maximize();
+            using (IWebDriver driver = new ChromeDriver(service, option))
+            {
+                driver.Navigate().GoToUrl(" http://172.31.7.80:8081/WS_ACK.asmx?WSDL");
+                Thread.Sleep(3000);
+                List<IWebElement> list = driver.FindElements(By.CssSelector("body div div div div table tbody tr")).ToList();
+                Thread.Sleep(3000);
+                foreach (IWebElement x in list)
+                {
+                    MessageBox.Show("뭔가 가져왔다");
+                    try
+                    {
+                        List<IWebElement> list1 = x.FindElements(By.CssSelector("td")).ToList();
+                        MessageBox.Show("뭔가 가져왔다111111111111111111111111111111111     "+ list1.ToString());
+                        txtDisplay.Text = list1.ToString();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("에러발생 ==>  " + ex.Message);
+                        throw;
+                    }
+                }
+            }
+
+
+
+
+            Thread.Sleep(3000);
+
+            //_driver.FindElement(By.Name("q")).SendKeys(txt_search.Text.ToString() + " 가사");
+            
+            //Thread.Sleep(3000);
+            //_driver.FindElement(By.Name("btnK")).Click();
+            //txtDisplay.Text = "";
+            //Thread.Sleep(5000);
+            //_driver.FindElement(By.XPath("//*[@id=\"tsuid12\"]/g-more-link/div")).Click();
+            //Thread.Sleep(1000);
+            //var text1 = _driver.FindElement(By.XPath("//*[@id=\"tsuid12\"]/span/div/div/div[2]/div/div/div/div/div/div[1]"));
+            
+            /////html/body/div[7]/div[2]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/g-more-link/div/span[2]/span[1]
+            
+            //String text2 = text1.Text;
+            //txtDisplay.Text = text2;
+
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+    }
+}
